a18c78d2f2d39e63646f9c530c8b1212ef10a6f3
KamilArici <kamilarici5@gmail.com>
2023-06-01 18:09:21 +0300

    array çalışma

--- array.js
+++ array.js
@@ -13,24 +13,93 @@ const ogrenciler = [
   { id: 44, isim: "ceren", yas: 30 },
 ];
 
-let a = sayilar.shift();
-console.log(a);
-console.log(sayilar);
-let c = ogrenciler.shift();
-console.log(c);
-console.log(ogrenciler);
+// let c = ogrenciler.push({ id: 15, isim: "kml", yas: 33 });
+// console.log(c);
+// console.log(ogrenciler);
+// // let a = sayilar.map((item) => item * 3);
+// console.log(a);
+// let a = isimler.concat(sayilar);
+// console.log(a);
+// let a = ogrenciler.filter((x) => x.yas > 20 && x.yas < 46);
+// console.log(a);
+// let a = ogrenciler.find((item) => item.yas > 30);
+// console.log(a);
+// console.log(ogrenciler);
+// let a = ogrenciler.filter((item) => item.id % 2 === 1);
+// console.log(a);
+// console.log(ogrenciler);
+// let a = sayilar.forEach(function (sayi) {
+//   console.log(sayi * 2);
+// });
+// console.log(sayilar);
+// let a = isimler.includes(`ali`);
+// console.log(a);
 
-// !DEC
-function ahmet() {
-  console.log("aaaaa");
-}
+// let a = ogrenciler.find((item) => item.id === 32);
+// console.log(a);
+// console.log(ogrenciler);
 
-// ! EXP
-const mehmet = function () {
-  console.log("aaaaa");
-};
+// let a = isimler.splice(1);
+// console.log(a);
+// console.log(isimler);
 
-// ! ARROW
-const fatma = () => {
-  console.log("aaaaa");
-};
+// let a = sayilar.splice(2, 0, 44, 55);
+// console.log(a);
+// console.log(sayilar);
+
+// let b = isimler.slice(1, 3);
+// console.log(b);
+// console.log(isimler);
+// let a = sayilar.slice(1, 5);
+// console.log(a);
+// console.log(sayilar);
+
+// let c = ogrenciler.push('id: 15, isim:"kml", yas: 33');
+// console.log(c);
+// console.log(ogrenciler);
+// let a = sayilar.push(2222);
+// console.log(a);
+// console.log(sayilar);
+
+// let b = isimler.push("kamil");
+// console.log(b);
+// console.log(isimler);
+// let c = ogrenciler.pop();
+// console.log(c);
+// console.log(ogrenciler);
+// let d = sayilar.unshift(100);
+// console.log(d);
+// console.log(sayilar);
+
+// let e = isimler.unshift("noah");
+// console.log(isimler);
+
+// let a = sayilar.pop();
+// console.log(a);
+// console.log(sayilar);
+
+// let b = isimler.pop();
+// console.log(b);
+// console.log(isimler);
+// let f=
+// let a = sayilar.shift();
+// console.log(a);
+// console.log(sayilar);
+// let c = ogrenciler.shift();
+// console.log(c);
+// console.log(ogrenciler);
+
+// // !DEC
+// function ahmet() {
+//   console.log("aaaaa");
+// }
+
+// // ! EXP
+// const mehmet = function () {
+//   console.log("aaaaa");
+// };
+
+// // ! ARROW
+// const fatma = () => {
+//   console.log("aaaaa");
+// };
